45f52e9bc8954f1f49eb235c302d0ea7cecbe51a
joneqian <qian.qing@aliyun.com>
2016-02-22 15:57:33 +0800

    my page

---
+++ public/js/controller/user-ctrl.js
@@ -0,0 +1,35 @@
+/**
+ * @author qianqing
+ * @create by 16-2-22
+ * @description user controller
+ */
+require.config({
+  baseUrl: '../js',
+  paths: {
+    'Vue': './lib/vue.min'
+  },
+  shim: {
+    'Vue': {
+      exports: 'Vue'
+    }
+  }
+});
+
+require(['Vue'],
+  function (Vue) {
+    'use strict';
+    Vue.config.delimiters = ['${', '}'];
+    Vue.config.unsafeDelimiters = ['{!!', '!!}'];
+
+    $(document).on("pageInit", "#page-my", function (e, id, page) {
+      var vm = new Vue({
+        el: '#page-my',
+        data: {
+        }
+      });
+
+    });
+
+    $.init();
+  }
+);
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('my', { title: '我的-好好卖' });
 });
 
 module.exports = router;
