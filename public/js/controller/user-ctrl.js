d0fe3a3b6220c533552ab587b17714ba740da045
joneqian <qian.qing@aliyun.com>
2016-02-22 18:07:36 +0800

    my page

--- public/js/controller/user-ctrl.js
+++ public/js/controller/user-ctrl.js
@@ -30,6 +30,54 @@ require(['Vue'],
 
     });
 
+    $(document).on("pageInit", "#page-my-account", function (e, id, page) {
+      $("title").text('账户管理');
+      var vm = new Vue({
+        el: '#page-my-account',
+        data: {
+        }
+      });
+
+      $(page).on('click', '#linkName', function () {
+        $.router.load('/users/change-shop-name');
+      });
+
+      $(page).on('click', '#linkType', function () {
+        $.router.load('/users/change-shop-style');
+      });
+
+    });
+
+    $(document).on("pageInit", "#page-change-shop-name", function (e, id, page) {
+      $("title").text('店铺名称');
+      var vm = new Vue({
+        el: '#page-change-shop-name',
+        data: {
+        }
+      });
+
+      $(page).on('click', '.button', function () {
+        $.router.load('/users/account');
+      });
+
+    });
+
+    $(document).on("pageInit", "#page-change-shop-style", function (e, id, page) {
+      $("title").text('店铺类型');
+      var vm = new Vue({
+        el: '#page-change-shop-style',
+        data: {
+        }
+      });
+
+      $(page).on('click', 'li a', function () {
+        $('li a').removeClass('my-shop-active');
+        $(this).addClass('my-shop-active');
+        $.router.load('/users/account');
+      });
+
+    });
+
     $.init();
   }
 );
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('my', { title: '我的-好好卖' });
 });
 
+router.get('/account', function(req, res, next) {
+  res.render('my-account', { title: '账户管理-好好卖' });
+});
+
+router.get('/change-shop-name', function(req, res, next) {
+  res.render('change-shop-name', { title: '店铺名称-好好卖' });
+});
+
+router.get('/change-shop-style', function(req, res, next) {
+  res.render('change-shop-style', { title: '店铺名称-好好卖' });
+});
+
 module.exports = router;
