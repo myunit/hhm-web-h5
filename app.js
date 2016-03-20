800a704daf1d33a85b2aaa28887e5934be436dd9
joneqian <qian.qing@aliyun.com>
2016-03-20 12:47:51 +0800

    cookie session

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var compression = require('compression');
 var session = require('express-session');
+var cookieSession = require('cookie-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
