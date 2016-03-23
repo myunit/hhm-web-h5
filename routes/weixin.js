54ccd72e6f1e3280b816fbf6d38f1cc8d8e8d335
joneqian <qian.qing@aliyun.com>
2016-03-23 18:39:24 +0800

    weixin

--- routes/weixin.js
+++ routes/weixin.js
@@ -7,16 +7,22 @@ var express = require('express');
 var unirest = require('unirest');
 var router = express.Router();
 var nwo = require('node-weixin-oauth');
-var wx_conf = require('../weixinPay.json');
+var wx_conf = require('../weixinPay');
 
 router.get('/code', function (req, res, next) {
-  var url = nwo.createURL(wx_conf.appId, 'http://joneqian.top/weixin/oauth', req.query.productId, 0);
+  var url = nwo.createURL(wx_conf.appId, wx_conf.authUrl, req.query.productId, 0);
   console.log(url);
   res.redirect(url);
 });
 
 router.get('/oauth', function (req, res, next) {
-  nwo.success(app, code, function(error, body) {
+  var app = {
+    id: wx_conf.appId,
+    secret: wx_conf.secret,
+    token: 'weixin'
+  };
+
+  nwo.success(app, req.query.code, function(error, body) {
     if (!error) {
       var accessToken = body.acess_token;
       var refreshToken = body.refresh_token;
