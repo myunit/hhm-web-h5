d6a12c57415cf411d05e77b2a4c85581da0fa5ed
joneqian <qian.qing@aliyun.com>
2016-03-31 22:14:29 +0800

    fix bug

--- routes/alipay.js
+++ routes/alipay.js
@@ -13,27 +13,32 @@ var orderApi = ApiFactory.CreateApi('order');
 
 router.post('/pay-notify-app', function (req, res, next) {
   var trade_status = req.body.trade_status;//交易状态
+  var data = req.body;
   libSign.verity(req.body, function(result) {
-    console.log('result: ' + result);
     if (result) {
-      console.log('trade_status: ' + trade_status);
-      console.log('query: ' + JSON.stringify(req.body));
-      if( trade_status == "TRADE_FINISHED") {
-        // 判断该笔订单是否在商户网站中已经做过处理
-        // 如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
-        // 如果有做过处理，不执行商户的业务程序
+      if (trade_status=="TRADE_SUCCESS") {
+        var out_trade_no = data['out_trade_no'];
+        out_trade_no = out_trade_no.split('_');
+        var userId = parseInt(out_trade_no[0]);
+        var orderId = parseInt(out_trade_no[1]);
 
-        // 注意：
-        // 该种交易状态只在两种情况下出现
-        // 1、开通了普通即时到账，买家付款成功后。
-        // 2、开通了高级即时到账，从该笔交易成功时间算起，过了签约时的可退款时限（如：三个月以内可退款、一年以内可退款等）后。
-      } else if (trade_status=="TRADE_SUCCESS") {
-        // 判断该笔订单是否在商户网站中已经做过处理
-        // 如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
-        // 如果有做过处理，不执行商户的业务程序
+        var userName = data['buyer_email'];
+        var total = data['total_fee']*100/100;
 
-        // 注意：
-        // 该种交易状态只在一种情况下出现——开通了高级即时到账，买家付款成功后。
+        var obj = {
+          "userId": userId,
+          "orderId": orderId,
+          "note": "支付宝支付",
+          "buyer": userName,
+          "total": total,
+          "tradeId": data['trade_no'],
+          "type": 14
+        };
+        unirest.post(orderApi.createPaymentRecord())
+          .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+          .send(obj)
+          .end(function (response) {
+          });
       }
 
       res.end("success");
